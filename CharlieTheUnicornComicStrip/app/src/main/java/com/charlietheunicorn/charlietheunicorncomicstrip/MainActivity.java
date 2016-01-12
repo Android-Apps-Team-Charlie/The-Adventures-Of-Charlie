78dc18b6c5b0333d4ac34dfa2cb5b5bb07069d2e
Margarita Ivancheva <mvivancheva@gmail.com>
2016-01-12 18:29:56 +0200

    background update plus added custom fonts

--- CharlieTheUnicornComicStrip/app/src/main/java/com/charlietheunicorn/charlietheunicorncomicstrip/MainActivity.java
+++ CharlieTheUnicornComicStrip/app/src/main/java/com/charlietheunicorn/charlietheunicorncomicstrip/MainActivity.java
@@ -1,16 +1,26 @@
 package com.charlietheunicorn.charlietheunicorncomicstrip;
 
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.widget.TextView;
 
 import com.charlietheunicorn.charlietheunicorncomicstrip.fragments.MainActivityFragment;
 
+import java.util.Locale;
+
 public class MainActivity extends FragmentActivity {
+
+    Context context;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.initial);
+
+
 
         if (findViewById(R.id.fragment_container) != null) {
 
--- CharlieTheUnicornComicStrip/app/src/main/java/com/charlietheunicorn/charlietheunicorncomicstrip/fragments/MainActivityFragment.java
+++ CharlieTheUnicornComicStrip/app/src/main/java/com/charlietheunicorn/charlietheunicorncomicstrip/fragments/MainActivityFragment.java
@@ -1,5 +1,7 @@
 package com.charlietheunicorn.charlietheunicorncomicstrip.fragments;
 
+import android.content.Context;
+import android.graphics.Typeface;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
@@ -7,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.charlietheunicorn.charlietheunicorncomicstrip.R;
 import com.charlietheunicorn.charlietheunicorncomicstrip.fragments.StartGameFragment;
@@ -23,9 +26,19 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        Context context = getActivity();
+
         View view = inflater.inflate(R.layout.fragment_main, container, false);
 
+        TextView tx = (TextView)view.findViewById(R.id.tv_welcome);
+
+        Typeface typeface = Typeface.createFromAsset(context.getAssets(), "fonts/comics.ttf");
+
+        tx.setTypeface(typeface);
+
         Button button_start = (Button) view.findViewById(R.id.button_start);
+
+        button_start.setTypeface(typeface);
         button_start.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
